db4fae81288c3489d0125d4398965c179c6e3972
Jeremy Guery <j.guery226@gmail.com>
2022-08-12 10:46:53 +0200

    ajout de express server app

--- back/server.js
+++ back/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
+const app = require('./app');
 
-const server = http.createServer((req, res, next) => {
-    res.end('Réponse server OK');
-});
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);

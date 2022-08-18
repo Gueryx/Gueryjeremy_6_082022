b4223b693f02dbe38ce4ec7050ff80f2cc95c6ce
Jeremy Guery <j.guery226@gmail.com>
2022-08-18 13:25:15 +0200

    correction schema de données

--- models/Thing.js
+++ models/Thing.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 // Création d'un schéma de données
 const thingSchema = mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    imageUrl: { type: String, required: true },
     userId: { type: String, required: true },
-    price: { type: Number, required: true },
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true, min: "1", max: "10" },
 });
 
 //Exporter le model 

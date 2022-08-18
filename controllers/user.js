82077a8fc14497eda9f94cd1a6d6e9c014fe9aca
Jeremy Guery <j.guery226@gmail.com>
2022-08-18 10:35:58 +0200

    vérifications des informations d'identification d'un utilisateur

--- controllers/user.js
+++ controllers/user.js
@@ -25,5 +25,24 @@ exports.signup = (req, res, next) => {
 
 // Fonction login pour connecter les utilisateurs existant
 exports.login = (req, res, next) => {
-
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user === null) {
+                res.status(401).json({ message: 'Identifiant/mot de passe incorrecte' });
+            } else {
+                bcrypt.compare(req.body.password, user.password)
+                    .then(valid => {
+                        if (!valid) {
+                            res.status(401).json({ message: 'Identifiant/mot de passe incorrecte' });
+                        } else {
+                            res.status(200).json({
+                                userId: user._id,
+                                token: 'TOKEN'
+                            });
+                        }
+                    })
+                    .catch(error => res.status(500).json({ error }));
+            }
+        })
+        .catch(error => res.status(500).json({ error }));
 };

35cd2e460adc7845fb266bcc1ac24325d6b893cd
Jeremy Guery <j.guery226@gmail.com>
2022-08-30 10:15:30 +0200

    aller chercher l'objet dans la base de donnée pour like/dislike

--- controllers/like.js
+++ controllers/like.js
@@ -2,5 +2,13 @@
 const userSchema = require("../models/User");
 
 exports.likeThing = (req, res, next) => {
+    // Mettre un _ à l'id et aller chercher le produit dans la base de donnée
+    userSchema.findOne({ _id: req.params.id })
+        .then((product) => {
+            res.status(200).json(product);
+        })
+        .catch((error) => res.status(404).json({ error }));
+
+    // Like +1 
 
 }
